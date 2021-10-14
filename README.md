ad0b0577a5192948fae0da93fbc1f31dfccac118
ajbalogh <andy.balogh@keysight.com>
2021-10-14 14:02:30 -0700

    updates

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# ONEx models
-This ONEx repository produces OpenAPI artifacts that describe fabric and dataflow APIs and Data Models neccessary for creating open network experiments.
+# ONEx API and Data Models
+This ONEx repository produces OpenAPI artifacts that describe APIs and Data Models neccessary for creating open network experiments.
 
-Get started with these links.
-- [Experiments using Go](GO.md)
-- [Experiments using Python](PYTHON.md)
-- [Fabric API and Data Model Documentation](./artifacts/fabric_openapi.html)
-- [Dataflow API and Data Model Documentation](./artifacts/dataflow_openapi.html)
+The repository contains API and Data Models for the following areas:
+- servers
+- fabric
+- dataflow
+- chaos
+
+Current versions of the API and Data Modelscan be viewed using the these links:
+- [Fabric API and Data Model Documentation](./docs/fabric_openapi.html)
+- [Dataflow API and Data Model Documentation](./docs/dataflow_openapi.html)
 
 
--- generate.py
+++ generate.py
@@ -5,20 +5,20 @@ import openapiart
 
 def generate():
     files = {
-        # "fabric": ["./models/common.yaml", "./models/fabric/api.yaml"],
-        "dataflow": ["./models/common.yaml", "./models/dataflow/api.yaml"],
+        "fabric": ["./common/common.yaml", "./fabric/api.yaml"],
+        "dataflow": ["./common/common.yaml", "./dataflow/api.yaml"],
     }
     for package_name, file_names in files.items():
         art = openapiart.OpenApiArt(
             api_files=file_names,
             protobuf_name=package_name + "pb",
-            artifact_dir=os.path.join(os.path.dirname(__file__), package_name),
+            artifact_dir=os.path.join(os.path.dirname(__file__), "artifacts", package_name),
         )
-        art.GeneratePythonSdk(package_name=package_name)
-        art.GenerateGoSdk(
-            package_dir="github.com/ajbalogh/distributed-system-emulation/go{}".format(package_name),
-            package_name="go" + package_name,
-        )
+        # art.GeneratePythonSdk(package_name=package_name)
+        # art.GenerateGoSdk(
+        #     package_dir="github.com/ajbalogh/open-network-experiments/go{}".format(package_name),
+        #     package_name="go" + package_name,
+        # )
 
 
 if __name__ == "__main__":
