03c254c6dd45c1594d5f582d36f813285f085268
tameling <thomas.ameling@keysight.com>
2021-11-01 23:38:47 +0000

    added qos profiles (dscp mapping still pending)

--- pytests/fabric/test_basic_fabric.py
+++ pytests/fabric/test_basic_fabric.py
@@ -1,10 +1,8 @@
 from artifacts.fabric import fabric as onex
 
 def test_simple_fabric_with_oversubscription():
-    api = onex.api()
-    config = api.config()
+    config = onex.api().config()
 
-    config.fabric.choice = config.fabric.SPINE_POD_RACK
     config.fabric.spine_pod_rack.spines.add(count=1)
     config.fabric.spine_pod_rack.pods.add(count=2, pod_profile_name=["Pod Profile 1"])
 
@@ -18,8 +16,41 @@ def test_simple_fabric_with_oversubscription():
         tor_to_pod_oversubscription="2:1"
     )
 
-    storage_host = config.hosts.add(name="Data Storage 1", address="1.1.1.1")
-    compute1 = config.hosts.add(name="Compute 1", address="3.3.3.3")
-    compute2 = config.hosts.add(name="Compute 2", address="4.4.4.4")
+    config.hosts.add(name="Data Storage 1", address="1.1.1.1")
+    config.hosts.add(name="Compute 1", address="3.3.3.3")
+    config.hosts.add(name="Compute 2", address="4.4.4.4")
 
-    print(config.serialize())
+def test_qos_ingress_admission():
+    config = onex.api().config()
+
+    # Objective: Dedicate 1MB to each port in the pod switch
+
+    # 1. create a qos profile with the respective buffer settings
+    qos_profile = config.fabric.qos_profiles.add(name='restricted ingress admission')
+    qos_profile.ingress_admission.shared_buffer_bytes = 0
+    qos_profile.ingress_admission.reserved_buffer_bytes = 1 * 1024 * 1024
+
+    # 2. create a topology with a single pod (and 1 switch in the pod),
+    #    assign the qos profile to it
+    config.fabric.spine_pod_rack.pods.add(count=1, pod_profile_name=["Pod Profile 1"])
+    pod_profile = config.fabric.spine_pod_rack.pod_profiles.add(name="Pod Profile 1")
+    pod_profile.pod_switch.count = 1
+    pod_profile.pod_switch.qos_profile_name = qos_profile.name
+
+def test_qos_scheduling_discipline():
+    config = onex.api().config()
+
+    qos_profile = config.fabric.qos_profiles.add(name='restricted ingress admission')
+
+    # qos_profile.packet_classification.map_dscp_to_traffic_class = {
+    #     "10": "1",
+    #     "20": "2"
+    # }
+
+    qos_profile.scheduler.scheduler_mode = qos_profile.scheduler.WEIGHTED_ROUND_ROBIN
+    qos_profile.scheduler.weight_list = [1, 2, 4]
+
+    config.fabric.spine_pod_rack.pods.add(count=1, pod_profile_name=["Pod Profile 1"])
+    pod_profile = config.fabric.spine_pod_rack.pod_profiles.add(name="Pod Profile 1")
+    pod_profile.pod_switch.count = 1
+    pod_profile.pod_switch.qos_profile_name = qos_profile.name
