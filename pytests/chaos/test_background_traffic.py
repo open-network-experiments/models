ffaa244f29ba176d813c2cce94326dd5be932526
tameling <thomas.ameling@keysight.com>
2021-11-05 00:27:02 +0000

    added background traffic stateless packet description

--- pytests/chaos/test_background_traffic.py
+++ pytests/chaos/test_background_traffic.py
@@ -1,13 +1,22 @@
 from artifacts.datamodel import datamodel as onex
 
 def test_background_traffic():
-    config = onex.api().config()
+    api = onex.api()
+    config = api.config()
 
     flow1 = config.chaos.background_traffic.flows.add(name="Flow 1")
     flow1.fabric_entry_point.front_panel_port.front_panel_port = 1
     stateless_flow = flow1.stateless.add(name='UDP DSCP 46')
+    stateless_flow.packet.src_address = "1.1.1.1"
+    stateless_flow.packet.dst_address = "2.2.2.2"
+    stateless_flow.packet.src_port = 10000
+    stateless_flow.packet.dst_port = 50000
+    stateless_flow.packet.size = 1518
+    stateless_flow.packet.l4_protocol = 'udp'
     stateless_flow.rate = 20
     stateless_flow.rate_unit = 'Gbps'
 
     flow2 = config.chaos.background_traffic.flows.add(name="Flow 2")
     flow2.fabric_entry_point.switch_reference.spine.switch_index = 1
+
+    api.set_config(config)
