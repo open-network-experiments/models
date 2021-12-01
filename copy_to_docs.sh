0cfa66dde4f7d4f32f7fb2b1abf18652acc8a2e2
tameling <thomas.ameling@keysight.com>
2021-12-01 11:13:35 -0800

    added copy_to_docs.sh (to update /docs in dev build)

---
+++ copy_to_docs.sh
@@ -0,0 +1,6 @@
+cp $(pwd)/artifacts/datamodel/openapi.yaml docs/onexdatamodel_openapi.yaml
+cp $(pwd)/artifacts/datamodel/*.proto docs
+cp $(pwd)/artifacts/fabric/openapi.yaml docs/onexfabric_openapi.yaml
+cp $(pwd)/artifacts/fabric/*.proto docs
+cp $(pwd)/artifacts/dataflow/openapi.yaml docs/onexdataflow_openapi.yaml
+cp $(pwd)/artifacts/dataflow/*.proto docs
