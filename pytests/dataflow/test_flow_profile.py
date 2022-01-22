4dee2dc24dd24800b93dace7805d68e69d83da97
tameling <thomas.ameling@keysight.com>
2022-01-21 16:52:04 -0800

    removed grpc code

--- pytests/dataflow/test_flow_profile.py
+++ pytests/dataflow/test_flow_profile.py
@@ -1,10 +1,6 @@
 from typing import NamedTuple
 from artifacts.onex_model.onex_model import onex_model
 
-from google.protobuf.json_format import ParseDict
-import json
-from artifacts.onex_dataflowapi.onex_dataflowapi import onexdataflowapi_pb2 as pb2
-
 def test_flow_profile_tcp():
     config = onex_model.api().config()
     fp = config.dataflow.flow_profiles.add(name='Test F Profile', data_size=1)
@@ -17,9 +13,3 @@ def test_flow_profile_tcp():
     tcp.destination_port.single_value.value = 3000
 
     assert config.serialize()
-    model_config_dict = json.loads(config.serialize())
-    api_set_config_req_dict = {
-        "config": model_config_dict
-    }
-    config_req = pb2.SetConfigRequest()
-    ParseDict(api_set_config_req_dict, config_req) # type: ignore
