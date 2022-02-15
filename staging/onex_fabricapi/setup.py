cad6a4b1a27bc1235bb47d0ae315ded05e6b1288
tameling <thomas.ameling@keysight.com>
2022-02-15 15:33:50 -0800

    version 0.0.6

--- staging/onex_fabricapi/setup.py
+++ staging/onex_fabricapi/setup.py
@@ -4,7 +4,7 @@ To build distribution: python setup.py sdist bdist_wheel --universal
 import setuptools
 
 pkg = "onex_fabricapi"
-version = "0.0.5"
+version = "0.0.6"
 
 setuptools.setup(
     name=pkg,
