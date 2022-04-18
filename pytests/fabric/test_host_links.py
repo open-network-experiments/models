0cd0fe8fd32d159c89f5122d235d80c15e7a1abd
diagalan <diana.galan@keysight.com>
2022-04-18 19:14:02 +0000

    move l1 profiles from config.fabric to config and the profile reference to hosts

--- pytests/fabric/test_host_links.py
+++ pytests/fabric/test_host_links.py
@@ -1,6 +1,4 @@
-from os import link
 from artifacts.onex_model import onex_model as onex
-from artifacts.onex_model.onex_model.onex_model import FabricL1SettingsProfile, FabricL1SettingsProfileAutonegotiation, FabricL1SettingsProfileAutonegotiationDisabled, SwitchHostLink
 
 def test_autoneg():
     config = onex.api().config()
@@ -14,22 +12,25 @@ def test_autoneg():
     config.fabric.clos.pods.add(count=2, pod_profile_name="Pod Profile 1")
 
     # layer 1 setting profiles
-    l1_profile_1 = config.fabric.layer1_profiles.add(name='200G Autoneg')
-    l1_profile_1.link_speed = FabricL1SettingsProfile.SPEED_200_GBPS
-    l1_profile_1.autonegotiation.choice = FabricL1SettingsProfileAutonegotiation.ENABLED
+    l1_profile_1 = config.layer1_profiles.add(name='200G Autoneg')
+    l1_profile_1.link_speed = onex.L1SettingsProfile.SPEED_200_GBPS
+    l1_profile_1.autonegotiation.choice = onex.L1SettingsProfileAutonegotiation.ENABLED
     l1_profile_1.autonegotiation.enabled.advertise_fec = True
 
 
-    l1_profile_2 = config.fabric.layer1_profiles.add(name='100G Manual RS FEC')
-    l1_profile_2.link_speed = FabricL1SettingsProfile.SPEED_100_GBPS
-    l1_profile_2.autonegotiation.choice = FabricL1SettingsProfileAutonegotiation.DISABLED
-    l1_profile_2.autonegotiation.disabled.fec_mode = FabricL1SettingsProfileAutonegotiationDisabled.REED_SOLOMON
+    l1_profile_2 = config.layer1_profiles.add(name='100G Manual RS FEC')
+    l1_profile_2.link_speed = onex.L1SettingsProfile.SPEED_100_GBPS
+    l1_profile_2.autonegotiation.choice = onex.L1SettingsProfileAutonegotiation.DISABLED
+    l1_profile_2.autonegotiation.disabled.fec_mode = onex.L1SettingsProfileAutonegotiationDisabled.REED_SOLOMON
+
+    host1.l1_profile_name =l1_profile_1.name
+    host2.l1_profile_name =l1_profile_1.name
 
     # host link
     host1_link = config.fabric.clos.host_links.add(host_name=host1.name)
     host1_link.front_panel_port = 1
-    host1_link.host_type = SwitchHostLink.EXTERNAL
-    host1_link.l1_profile_name =l1_profile_1.name
+    host1_link.host_type = onex.SwitchHostLink.EXTERNAL
+    host1_link.spine = 1
 
     print(config.serialize())
     
