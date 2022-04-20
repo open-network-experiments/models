1232f14d9b2c6ab3ed426baa6d1db82f265c2076
diagalan <diana.galan@keysight.com>
2022-04-20 19:52:01 +0000

    update Insights.PortMirroring; version 0.0.21

---
+++ pytests/insights/test_port_mirroring.py
@@ -0,0 +1,8 @@
+from artifacts.onex_model import onex_model as onex
+
+def test_port_mirroring():
+    config = onex.api().config()
+    mirror = config.insights.port_mirroring.add(name="Ingress frames port")
+    mirror.mirror_type = onex.InsightsPortMirroring.INGRESS_FRAMES
+    mirror.source_port = "Spine Switch 1 Port 1"
+    mirror.destination_port = 10
