57f6597abaed9e2da0207ac48d4f2dc941297b0f
diagalan <diana.galan@keysight.com>
2022-04-21 18:57:22 +0000

    Version 0.0.22

--- staging/onex_dataflowapi/setup.py
+++ staging/onex_dataflowapi/setup.py
@@ -4,7 +4,7 @@ To build distribution: python setup.py sdist bdist_wheel --universal
 import setuptools
 
 pkg = "onex_dataflowapi"
-version = "0.0.21"
+version = "0.0.22"
 
 setuptools.setup(
     name=pkg,
