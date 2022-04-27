e98b43684a1177ec3ee1470ed34ce39272720bff
diagalan <diana.galan@keysight.com>
2022-04-27 18:14:51 +0000

    update State schema and get_state, set_state APIs

--- pytests/fabric/test_state.py
+++ pytests/fabric/test_state.py
@@ -1,23 +1,12 @@
 from artifacts.onex_model import onex_model as onex
 
-def test_state_request():
-    state_request = onex.StateRequest()
+def test_state():
+    state = onex.State()
 
-    state_request.chaos.background_traffic.flow_names = ["F1", "F2"]
-    state_request.chaos.background_traffic.state = 'started'
+    state.chaos.background_traffic.add(flow_names = ["F1", "F2"], state='started')
+    state.chaos.background_traffic.add(flow_names = ["F3"], state='stopped')
 
-    state_request.chaos.drop_frames.config_names = ["10 percent Link 2", "Min drop Link 1"]
-    state_request.chaos.drop_frames.state = 'stopped'
+    state.chaos.drop_frames.add(config_names = ["10 percent Link 2", "Min drop Link 1"], state = 'stopped')
 
-    state_request.serialize()
-
-
-def test_state_response():
-    state_response = onex.StateResponse()
-
-    state_response.chaos.background_traffic.started = ["F1", "F2"]
-    state_response.chaos.background_traffic.stopped = ["F3"]
-
-    state_response.chaos.drop_frames.stopped = ["10 percent Link 2", "Min drop Link 1"]
-    
-    state_response.serialize()
+    state.serialize()
+    print(state.serialize())
