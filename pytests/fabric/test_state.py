27032068106f693417db234decc25c3517efb618
diagalan <diana.galan@keysight.com>
2022-04-28 23:09:27 +0000

    update test

--- pytests/fabric/test_state.py
+++ pytests/fabric/test_state.py
@@ -2,11 +2,7 @@ from artifacts.onex_model import onex_model as onex
 
 def test_state():
     state = onex.State()
-
     state.chaos.background_traffic.add(flow_names = ["F1", "F2"], state='started')
     state.chaos.background_traffic.add(flow_names = ["F3"], state='stopped')
-
     state.chaos.drop_frames.add(config_names = ["10 percent Link 2", "Min drop Link 1"], state = 'stopped')
-
     state.serialize()
-    print(state.serialize())
