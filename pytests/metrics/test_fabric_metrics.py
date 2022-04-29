19fbb30e844f1f429e48ea47b31b042b52325480
diagalan <diana.galan@keysight.com>
2022-04-29 23:56:19 +0000

    update fabric metrics get_metrics API schema

--- pytests/metrics/test_fabric_metrics.py
+++ pytests/metrics/test_fabric_metrics.py
@@ -1,20 +1,35 @@
 from artifacts.onex_model import onex_model as onex
 
-def test_port_link_status_request():
+def test_port_metrics_request():
     metric_request_1 = onex.MetricsRequest()
-    metric_request_1.port_link_status.port_names = ['Pod Switch 1.1 Port 10', 'ToR Switch 1.1 Port 5']
-    metric_request_1.port_link_status.labels = ['speed', 'fec_mode']
+    metric_request_1.port_metrics.port_names = ['Pod Switch 1.1 Port 10', 'ToR Switch 1.1 Port 5']
 
     metric_request_2 = onex.MetricsRequest()
-    metric_request_2.port_link_status.port_names = ['1/0', '32/0']
-    metric_request_2.port_link_status.labels = ['speed', 'fec_mode']
+    metric_request_2.port_metrics.front_panel_ports = ['1/0', '32/0']
+    metric_request_2.port_metrics.select_metrics = ['frames_transmitted_broadcast', 'frames_received_broadcast']
 
     metric_request_1.serialize()
     metric_request_2.serialize()
 
 
-def test_port_link_status_response():
+def test_port_metrics_response():
     metric_response = onex.MetricsResponse()
-    metric_response.port_link_status.add(port_name='Pod Switch 1.1 Port 10', front_panel_port='24/2', enabled=True, link_status = 'link_up', speed='speed_100_gbps', fec_mode='RS-FEC')
-    metric_response.port_link_status.add(port_name='ToR Switch 1.1 Port 5', front_panel_port='5/0', enabled=True, link_status = 'link_down', speed='speed_100_gbps', fec_mode='RS-FEC')
-    metric_response.serialize()
+    port_metric_response1 = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 10')
+    port_metric_response1.metrics.link_status = 'link_up'
+    port_metric_response1.metrics.frames_transmitted_broadcast = 100
+    port_metric_response1.metrics.frames_received_broadcast = 100
+
+
+    port_metric_response2 = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 1')
+    port_metric_response2.metrics.link_status = 'link_down'
+    port_metric_response2.metrics.frames_transmitted_broadcast = 100
+    port_metric_response2.metrics.frames_received_broadcast = 100
+    port_metric_response2.config.front_panel_port = '1/0'
+    port_metric_response2.config.connected_to = 'Host 1'
+    port_metric_response2.config.link_name = 'Link T.1.1/1'
+    port_metric_response2.config.tx_switch = 'ToR Switch 1.1'
+    port_metric_response2.config.rx_switch = 'ToR Switch 1.1'
+    port_metric_response2.config.speed = 'speed_100_gbps'
+    port_metric_response2.config.fec_mode = 'rs_fec'
+    port_metric_response2.config.autoneg_mode = 'disabled'
+    print(metric_response.serialize())
