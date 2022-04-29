db5bba3e7ba334792302ed0d231c6ca6c4a20e35
diagalan <diana.galan@keysight.com>
2022-04-29 00:08:20 +0000

    add enabled property to Fabric.QosProfile.Pfc; version 0.0.28

--- staging/onex_fabricapi/setup.py
+++ staging/onex_fabricapi/setup.py
@@ -4,7 +4,7 @@ To build distribution: python setup.py sdist bdist_wheel --universal
 import setuptools
 
 pkg = "onex_fabricapi"
-version = "0.0.27"
+version = "0.0.28"
 
 setuptools.setup(
     name=pkg,
