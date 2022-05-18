a1c4ccee531682dfa3c126c95e4898de29bcc5b7
diagalan <diana.galan@keysight.com>
2022-05-18 23:18:53 +0000

    update port metrics; version 0.0.35

--- pytests/metrics/test_fabric_metrics.py
+++ pytests/metrics/test_fabric_metrics.py
@@ -14,13 +14,14 @@ def test_port_metrics_request():
 
 def test_port_metrics_response():
     metric_response = onex.MetricsResponse()
-    port_metric_response1 = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 10')
+    metric_response.port_metrics.timestamp = 1652915804
+    port_metric_response1 = metric_response.port_metrics.metrics.add(port_name='ToR Switch 1.1 Port 10')
     port_metric_response1.metrics.link_status = 'link_up'
     port_metric_response1.metrics.frames_transmitted_broadcast = 100
     port_metric_response1.metrics.frames_received_broadcast = 100
 
 
-    port_metric_response2 = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 1')
+    port_metric_response2 = metric_response.port_metrics.metrics.add(port_name='ToR Switch 1.1 Port 1')
     port_metric_response2.metrics.link_status = 'link_down'
     port_metric_response2.metrics.frames_transmitted_broadcast = 100
     port_metric_response2.metrics.frames_received_broadcast = 100
@@ -38,7 +39,8 @@ def test_flow_counters_port_metrics():
     metric_request.serialize()
 
     metric_response = onex.MetricsResponse()
-    port_metric_response = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 10')
+    metric_response.port_metrics.timestamp = 1652915804
+    port_metric_response = metric_response.port_metrics.metrics.add(port_name='ToR Switch 1.1 Port 10')
     port_metric_response.metrics.flow_counter_metrics.per_background_traffic_flow.add(flow_name="Flow 1", frames_transmitted=100)
     port_metric_response.metrics.flow_counter_metrics.per_background_traffic_flow.add(flow_name="Flow 2", frames_transmitted=200)
     metric_response.serialize()
@@ -52,7 +54,8 @@ def test_per_priority_group_port_metrics():
     metric_request.serialize()
 
     metric_response = onex.MetricsResponse()
-    port_metric_response = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 10')
+    metric_response.port_metrics.timestamp = 1652915804
+    port_metric_response = metric_response.port_metrics.metrics.add(port_name='ToR Switch 1.1 Port 10')
     port_metric_response.metrics.per_priority_group_metrics.add(priority_group=0, ingress_buffer_reserved_usage_current=100, ingress_buffer_shared_usage_peak=200)
     port_metric_response.metrics.per_priority_group_metrics.add(priority_group=3, ingress_buffer_reserved_usage_current=0)
     metric_response.serialize()
@@ -65,7 +68,8 @@ def test_per_egress_queue_metrics():
     metric_request.serialize()
 
     metric_response = onex.MetricsResponse()
-    port_metric_response = metric_response.port_metrics.add(port_name='ToR Switch 1.1 Port 10')
+    metric_response.port_metrics.timestamp = 1652915804
+    port_metric_response = metric_response.port_metrics.metrics.add(port_name='ToR Switch 1.1 Port 10')
     port_metric_response.metrics.per_egress_queue_metrics.add(queue_number=0, egress_buffer_reserved_usage_current=0)
     port_metric_response.metrics.per_egress_queue_metrics.add(queue_number=3, egress_buffer_reserved_usage_current=0, egress_buffer_total_usage_peak=100,multicast_buffer_total_usage_peak=0)
     metric_response.serialize()
