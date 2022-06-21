f50996304bc28a4c46d65c6b62a9a2bac35f6af0
leonard.savu <leonard.savu@keysight.com>
2022-06-21 22:33:55 +0300

    - tests fixed for new nic params

--- pytests/dataflow/test_nic_settings.py
+++ pytests/dataflow/test_nic_settings.py
@@ -6,12 +6,12 @@ def test_nic_parameters():
     config = onex_model.api().config()
 
     host = config.hosts.add(name="Host 1", address="1.1.1.1")
-    config.dataflow.host_management.add(
-        host_name=host.name,
-        management_address="192.168.0.100",
-        nic_name='eth1',
-        nic_rx_buffer=1024*1024*1024,
-        nic_tx_buffer=1024*1024*1024,
-        nic_speed=100000
+    host_management = config.dataflow.host_management.add(
+        host_name=host.name
     )
+    host_management.eth_nic.management_address="192.168.0.100"
+    host_management.eth_nic.nic_name='eth1'
+    host_management.eth_nic.nic_rx_buffer=1024*1024*1024
+    host_management.eth_nic.nic_tx_buffer=1024*1024*1024
+    host_management.eth_nic.nic_speed=100000
     assert config.serialize()
