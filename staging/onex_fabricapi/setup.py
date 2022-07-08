31f4677a4132e8d29057e3434c8b75c4197b2153
diagalan <diana.galan@keysight.com>
2022-07-08 18:42:31 +0000

    fix - add port metrics for frame length intervals of 4096_9216 and 9217_16383; version 0.0.44

--- staging/onex_fabricapi/setup.py
+++ staging/onex_fabricapi/setup.py
@@ -4,7 +4,7 @@ To build distribution: python setup.py sdist bdist_wheel --universal
 import setuptools
 
 pkg = "onex_fabricapi"
-version = "0.0.43"
+version = "0.0.44"
 
 setuptools.setup(
     name=pkg,
